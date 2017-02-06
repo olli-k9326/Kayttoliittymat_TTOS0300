d083759fa7ba07bb2cb69980d343c8a7c6174ceb
K9326 <K9326@student.jamk.fi>
2017-02-06 13:54:28 +0200

    Labra9 1 teht. tehty

---
+++ Kayttoliittymat/Kayttoliittymat/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kayttoliittymat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int car;
+        private int truck;
+        public MainWindow()
+        {
+            InitializeComponent();
+            car = 0;
+            truck = 0;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            car++;
+            textBlock.Text = car.ToString();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            truck++;
+            textBlock2.Text = truck.ToString();
+        }
+    }
+}
---
+++ Kayttoliittymat/L9T2_euro-markka-laskuri/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace L9T2_euro_markka_laskuri
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxEUR_Mk_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            textBoxEUR_Mk
+        }
+    }
+}

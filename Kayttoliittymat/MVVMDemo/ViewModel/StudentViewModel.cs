89e9977e358485c344cc376c5bac8a6641d96a8d
K9326 <K9326@student.jamk.fi>
2017-02-22 15:59:50 +0200

    updatus

--- Kayttoliittymat/MVVMDemo/ViewModel/StudentViewModel.cs
+++ Kayttoliittymat/MVVMDemo/ViewModel/StudentViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MVVMDemo.Model;
 using System.Collections.ObjectModel;
+using MySql.Data.MySqlClient;
 
 namespace MVVMDemo.ViewModel
 {
@@ -23,6 +24,46 @@ namespace MVVMDemo.ViewModel
             students.Add(new Student { FirstName = "Teppo", LastName = "Testaaja", Asioid = "K4354" });
             students.Add(new Student { FirstName = "Tomi", LastName = "Tötterström", Asioid = "L8433" });
             Students = students;
+        }
+        //metodi StudentViewModeliin jolla haetaan oppilastiedot mysql-palvemilta
+        public void LoadStudentsFromMysql()
+        {
+            try
+            {
+                ObservableCollection<Student> students = new ObservableCollection<Student>();
+                //luodaan yhteys labranetin mysql-palvelimelle
+                string connStr = GetMysqlConnectionString();
+                string sql = "SELECT firstname, lastname, asioid FROM student";
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    conn.Open();
+                    using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            MVVMDemo.Model.Student s = new Model.Student();
+                            s.FirstName = reader.GetString(0);
+                            s.LastName = reader.GetString(1);
+                            s.Asioid = reader.GetString(2);
+                            students.Add(s);
+                        }
+                        Students = students;
+                    }
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private string GetMysqlConnectionString()
+        {
+            string pw = "";  //"h8sS7j6Ydtv5unJ97tH0lUECOXeMTW1s"
+            /// haetaan salasana App.Config- kongfiguraatiotiedostosta
+            pw = MVVMDemo.Properties.Settings.Default.passu;
+            return string.Format("Data source=mysql.labranet.jamk.fi;Initial Catalog=K9326_3;user=K9326;password={0}", pw);
         }
     }
 }
